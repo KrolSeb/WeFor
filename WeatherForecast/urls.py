36a8c0ff8b2a10dfadfc966fce75f9cefa85fa64
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2019-03-18 12:33:07 +0100

    Added cities detailed weather information url patterns to urls.py.

--- WeatherForecast/urls.py
+++ WeatherForecast/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.render_main_view),
     path('pl/', views.render_polish_cities_view),
     path('world/', views.render_worldwide_cities_view),
+    path('pl/<polish_city_details>/', views.render_polish_city_details_view, name='polish_city_details'),
+    path('world/city/<world_city_details>/', views.render_worldwide_city_details_view, name='world_city_details'),
+
 ]
