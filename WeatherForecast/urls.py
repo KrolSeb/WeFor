1aab3156bc00fcdeafb36694f630e137ca07014c
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2019-03-18 12:03:04 +0100

    Added polish and worldwide cities views url patterns to urls.py.

--- WeatherForecast/urls.py
+++ WeatherForecast/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.render_main_view),
+    path('pl/', views.render_polish_cities_view),
+    path('world/', views.render_worldwide_cities_view),
 ]
