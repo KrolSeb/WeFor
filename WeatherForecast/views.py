c72bb1325ac0012777abb285a6906345d30cad1e
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2019-03-18 11:32:42 +0100

    Added code used to rendering main view in views.py.

--- WeatherForecast/views.py
+++ WeatherForecast/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+""" Render main application view (on start)
+:returns: HTML template
+"""
+def render_main_view(request):
+    path_to_view = 'weather/weather_main.html'
+    return render(request, path_to_view)
