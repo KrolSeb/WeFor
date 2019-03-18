b1fbb020b694542c06e4ce51f794adf9540f1a46
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2019-03-18 13:12:43 +0100

    Added css,js and image used in extended mode cities template.

---
+++ WeatherForecast/static/js/animations_cities_extended.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function() {
+    document.querySelector('.navbar-scroll-into-top').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector('.top').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.querySelector('.navbar-scroll-into-main-content').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector('.main-content').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.querySelector('.button-scroll-into-main-content').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector('.main-content').scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
