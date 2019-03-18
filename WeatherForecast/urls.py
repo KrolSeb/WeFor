7b7315beb4ac42fe72951b0c5b50b0738815a7d3
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2019-03-18 13:07:48 +0100

    Added extended mode cities weather information url pattern to urls.py.

--- WeatherForecast/urls.py
+++ WeatherForecast/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('world/', views.render_worldwide_cities_view),
     path('pl/<polish_city_details>/', views.render_polish_city_details_view, name='polish_city_details'),
     path('world/city/<world_city_details>/', views.render_worldwide_city_details_view, name='world_city_details'),
-
+    path('extended/', views.render_extended_mode_view)
 ]
